4e20463008cce659a5f51206a92e1bd9efca3c99
maciejs <maciej.sledzinski@cyfroweszkoly.pl>
2025-09-25 13:45:07 +0200

    p

--- src/Card.tsx
+++ src/Card.tsx
@@ -5,12 +5,14 @@ interface CardProps {
     image?: string;
 }
 
-export default function Card({ pokemon, type, image }: CardProps) {
+function Card({ pokemon, type, image }: CardProps) {
     return (
         <div className="card" style={{ border: "1px solid black", padding: "10px", margin: "10px" }}>
             <h2>{pokemon}</h2>
             <p>Typ: {type}</p>
-            {image && <img src={image} alt={pokemon} style={{ width: 100, height: 100 }} />}
+            {image && <img src={image} style={{ width: 100, height: 100 }} />}
         </div>
     );
 }
+
+export default Card;
